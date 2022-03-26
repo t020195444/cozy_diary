0dde1f0faadbba924afe620f89894b5bae69389e
姜雲瀚 <herry.ilinke@gmail.com>
2022-03-26 15:24:06 +0800

    整合Google登入可用版本

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,9 @@ class MyApp extends StatelessWidget {
         "loginpage": (context) => const LoginPage(),
         "homepage": (context) => HomePage(),
       },
-      home: HomePage(),
+      home: MyHomePage(
+        title: 'CozyDiary',
+      ),
       debugShowCheckedModeBanner: false,
     );
   }
@@ -38,7 +40,10 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   late VideoPlayerController _controller;
-  final GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['email']);
+  bool _isLoggedIn = false;
+  late GoogleSignInAccount _userObj;
+
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
 
   @override
   void initState() {
@@ -55,116 +60,141 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-          child: Stack(
-        children: <Widget>[
-          //背景影片
-          Center(
-              child: ConstrainedBox(
-            child: _controller.value.isInitialized
-                ? AspectRatio(
-                    aspectRatio: _controller.value.aspectRatio,
-                    child: VideoPlayer(_controller),
-                  )
-                : Container(),
-            constraints: const BoxConstraints.expand(),
-          )),
-
-          //黑色透明背景
-          Opacity(
-            opacity: 0.3,
-            child: ConstrainedBox(
-              child: Image.asset(
-                'asstes/images/Black.png',
-                fit: BoxFit.cover,
-              ),
-              constraints: const BoxConstraints.expand(),
-            ),
-          ),
-
-          //CozyDiart圖片
-          Center(
-              child: Align(
-            alignment: const Alignment(0.0, -0.35),
-            child: Image.asset('asstes/images/CozyDiary.png'),
-          )),
-
-          //登入按鈕
-          Center(
-            // ignore: deprecated_member_use
-            child: Align(
-                alignment: const Alignment(0.0, 0.45),
-                // ignore: deprecated_member_use
-                child: SizedBox(
-                    width: 250,
-                    // ignore: deprecated_member_use
-                    child: RaisedButton(
-                      onPressed: () async {
-                        await _googleSignIn.signIn();
-                        setState(() {});
-                      },
-                      child: const Text(
-                        "登入",
-                        style: TextStyle(
-                            color: Color.fromARGB(255, 255, 255, 255),
-                            fontSize: 17.0,
-                            fontWeight: FontWeight.w500),
-                      ),
-                      color: const Color.fromARGB(125, 255, 255, 255),
-                      shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(20))),
-                    ))),
-          ),
-
-          //選擇登入按鈕
-          Center(
-            child: Align(
-                alignment: const Alignment(0.0, 0.57),
-                child: TextButton(
+    return _isLoggedIn
+        ? Scaffold(
+            body: Column(
+            children: [
+              Image.network(_userObj.photoUrl!),
+              Text(_userObj.displayName!),
+              Text(_userObj.email),
+              TextButton(
                   onPressed: () {
-                    showModalBottomSheet(
-                        context: context,
-                        builder: (context) {
-                          return Container(
-                              height: 200,
-                              color: Colors.black,
-                              child: Center(
-                                  child: Stack(
-                                children: <Widget>[
-                                  const Align(
-                                      alignment: Alignment(0.0, -0.8),
-                                      child: Text(
-                                        "選擇登入方式",
-                                        style: TextStyle(color: Colors.white),
-                                      )),
-                                  Align(
-                                    alignment: const Alignment(0.0, 0.0),
-                                    child: Image.asset(
-                                        "asstes/images/icons8-facebook-48.png"),
-                                  ),
-                                  const Align(
-                                      alignment: Alignment(0.0, 0.3),
-                                      child: Text(
-                                        "Facebook",
-                                        style: TextStyle(color: Colors.white),
-                                      )),
-                                ],
-                              )));
-                        });
+                    _googleSignIn.signOut().then((value) {
+                      setState(() {
+                        _isLoggedIn = false;
+                      });
+                    }).catchError((e) {});
                   },
-                  child: const Text(
-                    "其他登入方式  >",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 15,
-                        fontWeight: FontWeight.w500),
-                  ),
+                  child: Text("Logout"))
+            ],
+          ))
+        : Scaffold(
+            body: Center(
+                child: Stack(
+              children: <Widget>[
+                //背景影片
+                Center(
+                    child: ConstrainedBox(
+                  child: _controller.value.isInitialized
+                      ? AspectRatio(
+                          aspectRatio: _controller.value.aspectRatio,
+                          child: VideoPlayer(_controller),
+                        )
+                      : Container(),
+                  constraints: const BoxConstraints.expand(),
                 )),
-          ),
-        ],
-      )),
-    );
+
+                //黑色透明背景
+                Opacity(
+                  opacity: 0.3,
+                  child: ConstrainedBox(
+                    child: Image.asset(
+                      'asstes/images/Black.png',
+                      fit: BoxFit.cover,
+                    ),
+                    constraints: const BoxConstraints.expand(),
+                  ),
+                ),
+
+                //CozyDiart圖片
+                Center(
+                    child: Align(
+                  alignment: const Alignment(0.0, -0.35),
+                  child: Image.asset('asstes/images/CozyDiary.png'),
+                )),
+
+                //登入按鈕
+                Center(
+                  // ignore: deprecated_member_use
+                  child: Align(
+                      alignment: const Alignment(0.0, 0.45),
+                      // ignore: deprecated_member_use
+                      child: SizedBox(
+                          width: 250,
+                          // ignore: deprecated_member_use
+                          child: RaisedButton(
+                            onPressed: () {
+                              _googleSignIn.signIn().then((userData) {
+                                setState(() {
+                                  _isLoggedIn = true;
+                                  _userObj = userData!;
+                                });
+                              }).catchError((e) {});
+                            },
+                            child: const Text(
+                              "登入",
+                              style: TextStyle(
+                                  color: Color.fromARGB(255, 255, 255, 255),
+                                  fontSize: 17.0,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                            color: const Color.fromARGB(125, 255, 255, 255),
+                            shape: const RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20))),
+                          ))),
+                ),
+
+                //選擇登入按鈕
+                Center(
+                  child: Align(
+                      alignment: const Alignment(0.0, 0.57),
+                      child: TextButton(
+                        onPressed: () {
+                          showModalBottomSheet(
+                              context: context,
+                              builder: (context) {
+                                return Container(
+                                    height: 200,
+                                    color: Colors.black,
+                                    child: Center(
+                                        child: Stack(
+                                      children: <Widget>[
+                                        const Align(
+                                            alignment: Alignment(0.0, -0.8),
+                                            child: Text(
+                                              "選擇登入方式",
+                                              style: TextStyle(
+                                                  color: Colors.white),
+                                            )),
+                                        Align(
+                                          alignment: const Alignment(0.0, 0.0),
+                                          child: Image.asset(
+                                              "asstes/images/icons8-facebook-48.png"),
+                                        ),
+                                        const Align(
+                                            alignment: Alignment(0.0, 0.3),
+                                            child: Text(
+                                              "Facebook",
+                                              style: TextStyle(
+                                                  color: Colors.white),
+                                            )),
+                                      ],
+                                    )));
+                              });
+                        },
+                        child: const Text(
+                          "其他登入方式  >",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                              fontWeight: FontWeight.w500),
+                        ),
+                      )),
+                ),
+              ],
+            )),
+          );
   }
 
   @override
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -9,46 +11,27 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   bool _isLoggedIn = false;
   late GoogleSignInAccount _userObj;
+
   final GoogleSignIn _googleSignIn = GoogleSignIn();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Codesundar")),
-      body: Container(
-        child: _isLoggedIn
-            ? Column(
-                children: [
-                  Image.network(_userObj.photoUrl!),
-                  Text(_userObj.displayName!),
-                  Text(_userObj.email),
-                  TextButton(
-                      onPressed: () {
-                        _googleSignIn.signOut().then((value) {
-                          setState(() {
-                            _isLoggedIn = false;
-                          });
-                        }).catchError((e) {});
-                      },
-                      child: Text("Logout"))
-                ],
-              )
-            : Center(
-                child: ElevatedButton(
-                  child: Text("Login with Google"),
-                  onPressed: () {
-                    _googleSignIn.signIn().then((userData) {
-                      setState(() {
-                        _isLoggedIn = true;
-                        _userObj = userData!;
-                      });
-                    }).catchError((e) {
-                      print(e);
-                    });
-                  },
-                ),
-              ),
-      ),
-    );
+        body: Column(
+      children: [
+        Image.network(_userObj.photoUrl!),
+        Text(_userObj.displayName!),
+        Text(_userObj.email),
+        TextButton(
+            onPressed: () {
+              _googleSignIn.signOut().then((value) {
+                setState(() {
+                  _isLoggedIn = false;
+                });
+              }).catchError((e) {});
+            },
+            child: const Text("Logout"))
+      ],
+    ));
   }
 }
